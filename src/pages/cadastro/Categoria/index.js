043b41546c700f8b6057d74f806a03fc3b5e7b49
Marcelo Bueno <marcelo.bueno@planejamento.gov.br>
2020-07-30 22:51:48 -0300

    Alteração para utilização dos dados ou local ou no Heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -21,7 +21,9 @@ function CadastroCategoria() {
   };
 
   useEffect(() => {
-    const URL_TOP = 'http://localhost:8080/categorias';
+    const URL_TOP = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://buenoflix.herokuapp.com/categorias';
 
     fetch(URL_TOP)
       .then(async (responseServer) => {
--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 import Template from '../../../components/Template';
 
 const CadastroVideo = () => (
-    <Template>
-        <h1>Cadastro de Vídeo</h1>
+  <Template>
+    <h1>Cadastro de Vídeo</h1>
 
-        <Link to="/cadastro/categoria">
-            Cadastrar Categoria
-        </Link>
-    </Template>
+    <Link to="/cadastro/categoria">
+      Cadastrar Categoria
+    </Link>
+  </Template>
 );
 
 export default CadastroVideo;
